9a20a478c193a13c47cc9485b2be859f57ee46b8
pavel <ponomarchuk@yandex.ru>
2020-08-18 18:42:03 +0500

    Начал задачку по сумме минимальных эл-тов массива

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import SumOfElements from "./sumOfElementsTask/SumOfElements";
 
 const App = () => (
     <div>
       <header>
-
+          <h1>Учебные задачки по JS</h1>
       </header>
+      <SumOfElements />
     </div>
 );
 
---
+++ src/sumOfElementsTask/SumOfElements.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//TODO разметка ввода и вывода результата
+const SumOfElements = () => (
+    <div>
+
+    </div>
+);
+
+export default SumOfElements;
