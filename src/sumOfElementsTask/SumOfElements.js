574898a50b013df601318e54cd3436b0cb382b0b
pavel <ponomarchuk@yandex.ru>
2020-08-19 18:03:46 +0500

    Накидал болванку по сумме мин. эл-в массива

--- src/sumOfElementsTask/SumOfElements.js
+++ src/sumOfElementsTask/SumOfElements.js
@@ -1,10 +1,57 @@
 import React from "react";
+//import processData from "./dataProcessing";
 
 //TODO разметка ввода и вывода результата
+
+const Result = ({result}) => (
+    <p>{result}</p>
+);
+
+//result - это свойство, точнее типа объект из одного поля
+
+/*
 const SumOfElements = () => (
     <div>
-
+        <Result result="0"/>
     </div>
-);
+);*/
+
+class SumOfElements extends React.Component {
+    state = {
+        inputString: "",
+        outputString: "",
+    }
+
+    render() {
+        this.state.outputString = this.processData("23 1 45 6 89");
+        const { inputString, outputString } = this.state;
+
+        return (
+            <div>
+                <Result result={ outputString }/>
+            </div>
+        );
+    }
+
+    processData(inputString) {
+        //TODO проверить основные варианты и граничные условия
+        let arr = this.parseToArray(inputString);
+        let sortedArr = this.getSortedArray(arr);
+        let result = sortedArr[0] + sortedArr[1];
+
+        return result;
+    }
+
+    parseToArray(inputString) {
+        //TODO реализовать конвертацию в массив
+        let resultArr = [23, 1, 45, 6, 89];
+        return resultArr;
+    }
+
+    getSortedArray(inputArr) {
+        //TODO реализовать сортировку
+        return [1, 6, 23, 45, 89];
+    }
+}
 
 export default SumOfElements;
---
+++ src/sumOfElementsTask/dataProcessing.js
@@ -0,0 +1,14 @@
+/*
+const processData = () => {
+    return "0";
+};
+
+ */
+let processData = function() {
+    return "0";
+
+}
+
+
+export default processData();
+
